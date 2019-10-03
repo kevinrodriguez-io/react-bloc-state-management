bcb30584e4193b4abb41452df8e4141e7bfb8cd7
Kevin Rodríguez <_@kevinrodriguez.io>
2019-10-03 00:47:12 -0600

    Bloc pattern in react.js

---
+++ src/bloc/todos/bloc.js
@@ -0,0 +1,22 @@
+import { BehaviorSubject } from 'rxjs';
+import { createContext } from 'react';
+import { Load } from "./event";
+import { Initial, Loading, Loaded } from "./state";
+
+export class Bloc {
+  constructor() {
+    this.observable = new BehaviorSubject(new Initial());
+  }
+
+  async dispatch(event) {
+    if (event instanceof Load) {
+      this.observable.next(new Loading());
+      const result = await fetch('https://jsonplaceholder.typicode.com/todos');
+      const todos =  await result.json();
+      this.observable.next(new Loaded(todos));
+    }
+  }
+
+}
+
+export const BlocContext = createContext();
